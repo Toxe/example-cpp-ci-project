29884a9d103bdc1fb6101746c4e9f6960c47db4e
Toxe <toxe@toxe.net>
2020-03-05 12:48:26 +0100

    Print compiler and C++ standard library versions

--- example_simple.cpp
+++ example_simple.cpp
@@ -2,5 +2,27 @@
 
 int main()
 {
-    std::cout << "Example is working." << std::endl;
+#ifdef __cplusplus
+    std::cout << "__cplusplus: " << __cplusplus << '\n';
+#endif
+
+#ifdef _MSC_VER
+    std::cout << "_MSC_VER: " << _MSC_VER << '\n';
+#endif
+
+#if defined(__clang_version__)
+    std::cout << "__clang_version__: " << __clang_version__ << '\n';
+#endif
+
+#if defined(__GNUC__) && !defined(__clang_version__)
+    std::cout << "__GNUC__: " << __GNUC__ << '\n';
+#endif
+
+#ifdef _LIBCPP_VERSION
+    std::cout << "_LIBCPP_VERSION: " << _LIBCPP_VERSION << '\n';
+#endif
+
+#ifdef _GLIBCXX_RELEASE
+    std::cout << "_GLIBCXX_RELEASE: " << _GLIBCXX_RELEASE << '\n';
+#endif
 }
