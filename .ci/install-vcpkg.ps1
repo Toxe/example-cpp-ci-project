a133c7244d1974791d731da3e55661005117705e
Toxe <toxe@toxe.net>
2020-03-07 18:42:24 +0100

    Add install-vcpkg.ps1 script to install Vcpkg on Windows

---
+++ .ci/install-vcpkg.ps1
@@ -0,0 +1,28 @@
+# Build Vcpkg and install dependency packages.
+#
+# Usage: .ci\install-vcpkg.ps1 <project directory> [vcpkg directory name]
+# - project directory: Path to the project sources where the .vcpkg file is located.
+# - vcpkg directory name: optional name of directory where Vcpkg will be clone'd into
+#
+# Example 1: .ci\install-vcpkg.ps1 $Env:GITHUB_WORKSPACE
+# Example 2: .ci\install-vcpkg.ps1 $Env:APPVEYOR_BUILD_FOLDER vcpkg-msvc
+
+if ($args.Count -lt 1) { Exit 1 }
+
+$PROJECT_DIR=$args[0]
+$VCPKG_DIR=$args[1]
+
+if ($VCPKG_DIR -eq $null) { $VCPKG_DIR="vcpkg" }
+
+Write-Host "---- install-vcpkg.ps1"
+Write-Host "PROJECT_DIR:" $PROJECT_DIR
+Write-Host "VCPKG_DIR:" $VCPKG_DIR
+Write-Host "----------------------"
+
+Set-Location "$Env:HOMEPATH"
+New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe"
+git clone --quiet --depth 1 https://github.com/Microsoft/vcpkg.git $VCPKG_DIR
+Set-Location $VCPKG_DIR
+.\bootstrap-vcpkg.bat -disableMetrics
+$packages = Get-Content "$PROJECT_DIR\.vcpkg"
+.\vcpkg.exe install --triplet x64-windows $packages
