ad83968ef699a38b7197b2600e245f7a74e098c3
Toxe <toxe@toxe.net>
2022-06-04 10:56:37 +0200

    Update build scripts

--- .ci/build-project.ps1
+++ .ci/build-project.ps1
@@ -2,42 +2,43 @@
 #
 # The script assumes that it will be called from inside the project directory.
 #
-# Usage: .ci\build-project.ps1 [vcpkg-directory [build-directory-name]]
-# - vcpkg-directory: Optional full path to Vcpkg directory. Default: $HOME\vcpkg
+# Usage: .ci\build-project.ps1 [build-directory-name]
 # - build-directory-name: Optional name of build directory. Default: build.
-#                         Can only be set of vcpkg-directory is set as well.
 #
 # Example 1: .ci\build-project.ps1
-# Example 2: .ci\build-project.ps1 $HOME\vcpkg-clang
-# Example 3: .ci\build-project.ps1 $HOME\vcpkg-clang build-clang
+# Example 2: .ci\build-project.ps1 build-clang
 
-$ErrorActionPreference="Stop"
+$ErrorActionPreference = "Stop"
 
-$VCPKG_DIR=$args[0]
-$BUILD_DIR=$args[1]
+$BUILD_DIR = $args[0]
+$VCPKG_DIR = "$HOME\vcpkg"
 
-if ($null -eq $VCPKG_DIR) { $VCPKG_DIR="$HOME\vcpkg" }
-if ($null -eq $BUILD_DIR) { $BUILD_DIR="build" }
+if ($null -eq $BUILD_DIR)
+{
+    $BUILD_DIR = "build"
+}
 
 # only pass toolchain file to CMake if Vcpkg is installed
-if (Test-Path "$VCPKG_DIR" -PathType Container) {
-    $TOOLCHAIN="$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
-} else {
-    $TOOLCHAIN="False"
+if (Test-Path "$VCPKG_DIR" -PathType Container)
+{
+    $TOOLCHAIN = "$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
+}
+else
+{
+    $TOOLCHAIN = "False"
 }
 
 Write-Host "---- build-project.ps1 ----"
-Write-Host "VCPKG_DIR: $VCPKG_DIR"
 Write-Host "BUILD_DIR: $BUILD_DIR"
+Write-Host "VCPKG_DIR: $VCPKG_DIR"
 Write-Host "CMAKE_TOOLCHAIN_FILE: $TOOLCHAIN"
 Write-Host "---------------------------"
 
-if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
+if (-not(Get-Command cmake -ErrorAction SilentlyContinue))
+{
     New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
 }
 
-New-Item -Name $BUILD_DIR -ItemType Directory
-Push-Location $BUILD_DIR
-cmake -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" ..
-cmake --build . --config Release
-Pop-Location
+New-Item -Name "$BUILD_DIR" -ItemType Directory
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" -j 4 --config RelWithDebInfo
--- .ci/build-project.sh
+++ .ci/build-project.sh
@@ -4,33 +4,29 @@
 #
 # The script assumes that it will be called from inside the project directory.
 #
-# Usage: sh .ci/build-project.sh [vcpkg-directory [build-directory-name]]
-# - vcpkg-directory: Optional full path to Vcpkg directory. Default: $HOME/vcpkg
+# Usage: sh .ci/build-project.sh [build-directory-name]
 # - build-directory-name: Optional name of build directory. Default: build.
-#                         Can only be set of vcpkg-directory is set as well.
 #
 # Example 1: sh .ci/build-project.sh
-# Example 2: sh .ci/build-project.sh $HOME/vcpkg-clang
-# Example 3: sh .ci/build-project.sh $HOME/vcpkg-clang build-clang
+# Example 2: sh .ci/build-project.sh build-clang
 
 set -e
 
-VCPKG_DIR=${1:-$HOME/vcpkg}
-BUILD_DIR=${2:-build}
+BUILD_DIR=${1:-build}
+VCPKG_DIR="$HOME\vcpkg"
 
 # only pass toolchain file to CMake if Vcpkg is installed
 if [ -d "$VCPKG_DIR" ]; then TOOLCHAIN="$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"; else TOOLCHAIN=False; fi
 
 echo "---- build-project.sh ----"
-echo "VCPKG_DIR: $VCPKG_DIR"
 echo "BUILD_DIR: $BUILD_DIR"
+echo "VCPKG_DIR: $VCPKG_DIR"
 echo "CC: $CC"
 echo "CXX: $CXX"
 echo "CXXFLAGS: $CXXFLAGS"
 echo "CMAKE_TOOLCHAIN_FILE: $TOOLCHAIN"
 echo "--------------------------"
 
-mkdir "$BUILD_DIR" && pushd "$BUILD_DIR"
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" ..
-cmake --build .
-popd
+mkdir "$BUILD_DIR"
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" -j 4
