9ed3c15a5fbfe199d9590c472e094608d571b5d7
Toxe <toxe@toxe.net>
2022-06-18 10:25:40 +0200

    Prevent CTest from adding additional CDash targets

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -1,9 +1,9 @@
-# enable testing with CTest
+# enable testing with CTest (but prevent CTest from adding additional CDash targets)
+set_property(GLOBAL PROPERTY CTEST_TARGETS_ADDED 1)
 include(CTest)
 
 # Catch2 v3
 find_package(Catch2 3 CONFIG REQUIRED)
-
 include(Catch)
 
 # FakeIt
